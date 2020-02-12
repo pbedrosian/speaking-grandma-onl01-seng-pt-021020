24e9c3745d49ba753f9199e77f701fda646d5675
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-12 19:05:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -26,15 +26,15 @@
 #end
 #end
 
-def speak_to_grandma(phrase = "Hi")
- if phrase == "I LOVE YOU GRANDMA!"
-   "I LOVE YOU TOO PUMPKIN!"
-   elsif phrase == phrase
-   "HUH?! SPEAK UP, SONNY!"
-   elsif phrase == phrase
-  "NO, NOT SINCE 1938!"
-  end
-end
+#def speak_to_grandma(phrase = "Hi")
+ #if phrase == "I LOVE YOU GRANDMA!"
+  # "I LOVE YOU TOO PUMPKIN!"
+  # elsif phrase == phrase
+   #"HUH?! SPEAK UP, SONNY!"
+   #elsif phrase == phrase
+  #"NO, NOT SINCE 1938!"
+  #end
+#end
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
