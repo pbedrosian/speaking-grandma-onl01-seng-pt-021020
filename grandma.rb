2ffcfd23bc960ca2cb5ea571258bf9ff3430464a
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-12 19:03:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -36,6 +36,16 @@ def speak_to_grandma(phrase = "Hi")
   end
 end
 
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938!"
+  end
+end
+
 
 
 
